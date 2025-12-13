b6398e17ae44b8c19bd1db5ce4e764553fba9b78
BaptistaJose <baptistaj133@gmail.com>
2025-12-13 15:11:34 -0300

    Checklist 3: Autorización (Roles + Seguridad) y conexion a la Base de datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,36 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { secretaryModule } from './secretaria/secretary.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config as dotenvConfig } from "dotenv"
+import { JwtModule } from '@nestjs/jwt';
 
+dotenvConfig({ path: './.env.development' })
 
 @Module({
-  imports: [UserModule, secretaryModule],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const typeormConfig = configService.get('typeorm')
+        if (!typeormConfig) {
+          throw new Error('TypeORM configuration not found');
+        }
+        return typeormConfig
+      }
+    }), UserModule, secretaryModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
