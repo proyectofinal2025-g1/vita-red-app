192bbde46af3c6365de3ff349e089cdadb35b708
Jose Baptista <baptistaj133@gmail.com>
2025-12-26 15:11:19 -0300

    Revert "Feature/update login"

--- frontend/src/interfaces/IUserSession.ts
+++ frontend/src/interfaces/IUserSession.ts
@@ -1,13 +1,13 @@
 export interface IUserSession {
-  login: boolean;
-  token: string;
-  user: {
-    id: string; // era number antes de la actualización //
-    first_name: string; //
-    last_name: string; //
-    email: string; //
-    role: string; //
-    dni?: string; // era number antes de la actualización
-    appointments: []; //
-  };
+    login: boolean;
+    token: string;
+    user?: {
+        id: number;
+        first_name: string;
+        last_name: string;
+        dni: number;
+        email: string;
+        appointments: [];
+        role: string;
+    };
 }
