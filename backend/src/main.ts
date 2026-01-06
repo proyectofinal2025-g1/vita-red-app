569da80a0b4772f24dc7d5c6b986fc7889e49b88
BaptistaJose <baptistaj133@gmail.com>
2026-01-06 18:21:52 -0300

    actualizacion del cors para las peticiones del front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
   });
 
