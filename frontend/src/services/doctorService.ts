454d3258e10a3016ea0666fec9c03d5188367a6b
heberzan <98982221+heberzan@users.noreply.github.com>
2026-01-18 14:18:56 -0500

    feat: actualice el servicio de doctores, faltaba la 's'

--- frontend/src/services/doctorService.ts
+++ frontend/src/services/doctorService.ts
@@ -1,21 +1,21 @@
-import { IDoctor } from "@/interfaces/IDoctor";
+import { IDoctor } from '@/interfaces/IDoctor';
 const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getAllDoctorsService = async (): Promise<IDoctor[]> => {
   try {
-    const res = await fetch(`${apiURL}/doctor`, {
-      cache: "no-store",
-      method: "GET"
+    const res = await fetch(`${apiURL}/doctors`, {
+      cache: 'no-store',
+      method: 'GET',
     });
 
     if (!res.ok) {
-      throw new Error("Failed to fetch doctors");
+      throw new Error('Failed to fetch doctors');
     }
 
     const doctors: IDoctor[] = await res.json();
     return doctors;
   } catch (error) {
-    console.error("getAllDoctorsService error:", error);
-    throw new Error("Error fetching doctors");
+    console.error('getAllDoctorsService error:', error);
+    throw new Error('Error fetching doctors');
   }
 };
